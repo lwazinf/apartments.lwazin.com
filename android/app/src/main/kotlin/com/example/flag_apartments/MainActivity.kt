91fa76a7ff98f3b6283f9716f0d61a21236a19d9
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2021-03-14 22:23:45 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/flag_apartments/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flag_apartments
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/WrapperPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WrapperPage extends StatefulWidget {
+  @override
+  _WrapperPageState createState() => _WrapperPageState();
+}
+
+class _WrapperPageState extends State<WrapperPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
